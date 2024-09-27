462e5b75c1bded81a9842836e989646ce64b3125
Cristi <108606727+luvBB@users.noreply.github.com>
2024-09-27 19:40:47 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# Config file for HDBUP
+
+
+# TVDB
+api_key = 'YOUR-API-HERE'
+pin = 'YOUR-PIN-HERE'
+
+# Cookie values for hdbits.org
+cookie_values = {
+    'uid': 'YOUR-UID-HERE',
+    'pass': 'YOUR-PASS-HERE',
+    'hash': 'YOUR-HASH-HERE',
+    'hush': 'YOUR-HUSH-HERE'
+}
+
+# qBittorrent app
+qbittorrent_url = 'http://localhost:8089'
+qbittorrent_username = 'luvbb'
+qbittorrent_password = 'shortcake'
+
+# Apps paths
+mediainfo_path = r"F:\HDBUP-main\mediainfo\mediainfo.exe"
+ffmpeg_path = r"ffmpeg"
